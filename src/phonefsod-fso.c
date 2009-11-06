beb2fe989bfbf6b180a58d247db302082504fa01
Tom Hacohen <tom@nospam>
2009-11-06 18:38:26 +0200

    Return the phoneui dependency, though it should probably be removed and this should be handled in a more sane manner instead

--- src/phonefsod-fso.c
+++ src/phonefsod-fso.c
@@ -17,6 +17,7 @@
 #include <frameworkd-glib/odeviced/frameworkd-glib-odeviced-idlenotifier.h>
 #include <frameworkd-glib/odeviced/frameworkd-glib-odeviced-powersupply.h>
 #include <frameworkd-glib/odeviced/frameworkd-glib-odeviced-audio.h>
+#include <phoneui/phoneui.h>
 #include "phonefsod-dbus-phoneuid.h"
 #include "phonefsod-fso.h"
 #include "phonefsod-globals.h"
